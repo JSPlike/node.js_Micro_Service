c1a7a19231eb10ebc9466456b3bde182d4b75c3e
JSPlike <moter2588@gmail.com>
2019-03-17 15:24:46 +0900

    monolithic_mem_and_pur 0317 15:24

--- ch05/monolithic_purchases.js
+++ ch05/monolithic_purchases.js
@@ -1,3 +1,11 @@
+const mysql = require('mysql');
+const conn = {
+    host: 'localhost',
+    user: 'micro',
+    password: 'service',
+    database: 'monolithic'
+};
+
 exports = onRequest = function(res, method, pathname, params, cb){
 	switch (method) {
 		case "POST":
@@ -15,4 +23,60 @@ exports = onRequest = function(res, method, pathname, params, cb){
 		default:
 			return process.nextTick(cb, res, null);
 	}
+}
+
+function register(method, pathname, params, cb) {
+    var response = {
+        key: params.key,
+        errorcode: 0,
+        errormessage: "success"
+    };
+
+    if (params.userid == null || params.goodsid == null) {
+        response.errorcode = 1;
+        response.errormessage = "Invalid Parameters";
+        cb(response);
+    } else {
+        var connection = mysql.createConnection(conn);
+        connection.connect();
+        connection.query("insert into purchases(userid, goodsid) values(? ,? )"
+            , [params.userid, params.goodsid]
+            , (error, results, fields) => {
+                if (error) {
+                    response.errorcode = 1;
+                    response.errormessage = error;                    
+                } 
+                cb(response);
+            });
+        connection.end();
+    }
+}
+
+function inquiry(method, pathname, params, cb) {
+    var response = {
+        key: params.key,
+        errorcode: 0,
+        errormessage: "success"
+    };
+
+    if (params.userid == null) {
+        response.errorcode = 1;
+        response.errormessage = "Invalid Parameters";
+        cb(response);
+    } else {
+        var connection = mysql.createConnection(conn);
+        connection.connect();
+        connection.query("select id, goodsid, date from purchases where userid = ?"
+            , [params.userid]
+            , (error, results, fields) => {
+                if (error) {
+                    response.errorcode = 1;
+                    response.errormessage = error;
+                } else {
+                    response.results = results;
+                }
+                cb(response);
+            });
+        connection.end();
+    }
 }
